3c296bed5fee12ec3f6a3e21dd10f21c29a0e289
tisou1 <tisou1@qq.com>
2022-03-04 20:35:49 +0800

    chore: md

--- src/898-easy-includes/template.ts
+++ src/898-easy-includes/template.ts
@@ -1,17 +1,16 @@
-import { Equal, Expect } from '@type-challenges/utils'
 
-// type Includes<T extends readonly any[], U> = U extends  T[number] ? true : false
+type Includes<T extends readonly any[], U> = U extends  T[number] ? true : false
 
 
 // your answers
 // type Includes<T extends readonly any[], U> = T extends [infer F, ...infer R] ? Equal<F, U> extends true ? true : Includes<R, U> : false
 
-type Includes<T extends readonly any[], U> =
-  Equal<T[0], U> extends true
-    ? true
-    : T extends [T[0], ...infer R]
-      ? Includes<R, U>
-      : false;
+// type Includes<T extends readonly any[], U> =
+//   Equal<T[0], U> extends true
+//     ? true
+//     : T extends [T[0], ...infer R]
+//       ? Includes<R, U>
+//       : false;
 
 //js
 

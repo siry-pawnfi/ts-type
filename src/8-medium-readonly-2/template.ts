d269962eae788f6db4d9e5592f386f824f834b34
siry <82863607+siry-pawnfi@users.noreply.github.com>
2022-03-08 18:30:05 +0800

    chore: md 8-readonly-2

--- src/8-medium-readonly-2/template.ts
+++ src/8-medium-readonly-2/template.ts
@@ -1,12 +1,36 @@
 
+//T, K传入联合类型
+type Exclude2<T, K> = T extends K ? never : T 
+
+//T接口,K联合类型
+type Omit2<T, K extends keyof T> = {
+  [P in Exclude2<keyof T, K>] : T[P]
+} 
    
-// type MyReadonly2<T, K extends keyof T> =
-// {
-//   readonly [P in K] : T[P]
-// } & Omit<T,K>
+type MyReadonly2<T, K extends keyof T = never> = 
+{
+  readonly [P in K] : T[P]
+} & {
+  [P in keyof Omit<T, K>]: T[P]
+}
+
+type CCC = MyReadonly2<Todo1>
+interface Todo1 {
+  title: string
+  description?: string
+  completed: boolean
+}
+
 
 
 // type MyReadonly2<T, K extends keyof T> ={ [key in Exclude<keyof T, K>]: T[key] } & { readonly [key in K]: T[key]; }
+
+
+// K extends null ?
+// {
+//   readonly [P in keyof T]: T[P];
+// }
+// :
 
 // 实现一个通用MyReadonly2<T, K>，它带有两种类型的参数T和K。
 // K指定应设置为Readonly的T的属性集。如果未提供K，则应使所有属性都变为只读，就像普通的Readonly<T>一样。
@@ -27,3 +51,5 @@
 // }
 
 // 1.遍历对象,找到需要设置readonly的值
+
+type AA = Readonly<{name:string}>

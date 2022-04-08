3cfc7954e48cc0d1c0fc6efbf59a3b777c2955d3
tisou1 <57515396+tisou1@users.noreply.github.com>
2022-04-09 00:14:22 +0800

    chore: update

--- src/531-medium-string-to-union/test-cases.ts
+++ src/531-medium-string-to-union/test-cases.ts
@@ -1,9 +1,9 @@
+import { Equal, Expect } from '@type-challenges/utils'
 
-type StringToUnion<T extends string> = 
-  T extends `${infer F}${infer Rest}`
-    ? F | StringToUnion<Rest>
-    : never
+type cases = [
+  Expect<Equal<StringToUnion<"">, never>>,
+  Expect<Equal<StringToUnion<"t">, "t">>,
+  Expect<Equal<StringToUnion<"hello">, "h" | "e" | "l" | "l" | "o">>,
+  Expect<Equal<StringToUnion<"coronavirus">, "c" | "o" | "r" | "o" | "n" | "a" | "v" | "i" | "r" | "u" | "s">>,
+]
 
-
-//字符串转为union
-//infer 递归 

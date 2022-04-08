6064de48f01850eccd0cd175682184a0d5f3928b
tisou1 <57515396+tisou1@users.noreply.github.com>
2022-04-09 00:44:12 +0800

    chore: update 599

--- src/599-medium-merge/template.ts
+++ src/599-medium-merge/template.ts
@@ -1,9 +1,19 @@
 
 type Merge<F, S> = {
-  [K in (keyof F | keyof S)]: K extends keyof S ? S[K] : K extends keyof F ? F[K] : never
+  [K in (keyof F | keyof S)]: K extends keyof S ? S[K] :  K extends keyof F ? F[K] : never
 }
 
+//在前面的不会被覆盖,后面的会被覆盖
+type Foo1 = {
+  a: number;
+  b: string;
+};
+type Bar1 = {
+  b: number;
+  c: boolean;
+};
 
 
+type Sc = Merge<Foo1, Bar1>
 
 //关于interface和type进行扩展的问题
